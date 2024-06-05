18fd0e5c6dae66647c913625b7d8afbb7b91a5e7
Mauro <maurodesit@gmail.com>
2024-06-05 00:33:58 -0300

    Se agrega proyecto

--- App.js
+++ App.js
@@ -1,66 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { useState } from 'react';
-import { Button, StyleSheet, Text, View, TextInput } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+import StarGameScreen from './screens/StartGameScreen';
 
 export default function App() {
 
-  const [enteredgoalText,setEnteredGoal] =useState("");
-  const [courseGoals, setCourseGoals] = useState ([]);
-
-  function goalInputHandler(enteredText) {
-    setEnteredGoal(enteredText)
-  };
-   
-
-  function addGoalHandler() {
-    setCourseGoals((currentCourseGoals) =>[...currentCourseGoals,enteredgoalText]);
-  }
-
-  return (
-    <View style={styles.appContainer} >
-      <View style={styles.inputContainer}>
-        <TextInput style={styles.textImput} placeholder='Your course goal!' onChangeText={goalInputHandler}  /> 
-        <Button title='Add Goal' onPress={addGoalHandler}/>
-      </View>
-      
-      <View style={styles.goalsContainer}>
-        {courseGoals.map((goal)=><Text key={(goal)}>{goal}</Text>)}
-      </View>
-      
-    </View>
-  );
+return  <StarGameScreen/>
+  
 }
 
-const styles = StyleSheet.create({
-
-  appContainer:{
-    padding: 50,
-    paddingHorizontal:26,
-    flex:1
-
-
-  },
-
-  inputContainer:{
-    flex: 1,
-    flexDirection:"row",
-    justifyContent:"space-between",
-    alignItems: "center",
-    marginBottom:16,
-    borderBottomWidth:1,
-    borderBottomColor:"#cccccc"
-
-  },
-  textImput:{
-    borderWidth: 1,
-    borderColor:"#cccccc",
-    width:"70%",
-    marginRight:8,
-    padding: 8
-
-  },
-  goalsContainer:{
-    flex:5
-  }
-  
-});
+const styles = StyleSheet.create({});
---
+++ screens/StartGameScreen.js
@@ -0,0 +1,11 @@
+import{TextInput} from "react-native"
+import PrimaryButton from "../components/PrimaryButton";
+function StarGameScreen () {
+    return <View>
+        <TextInput/>
+        <PrimaryButton>Reset</PrimaryButton>
+        <PrimaryButton>Confirm</PrimaryButton>
+    </View>
+}
+
+export default StarGameScreen;
